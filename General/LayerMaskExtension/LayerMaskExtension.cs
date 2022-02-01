ab8f08d7959261d33668916af10b9603ae783dfe
DiscreteTom <discrete_tom@outlook.com>
2022-02-01 14:56:48 +0800

    optimize comments

--- General/LayerMaskExtension/LayerMaskExtension.cs
+++ General/LayerMaskExtension/LayerMaskExtension.cs
@@ -4,6 +4,7 @@ namespace DT.General
 {
   public static class LayerMaskExtension
   {
+    // ref: http://answers.unity.com/answers/1332280/view.html
     public static bool Contains(this LayerMask mask, int target)
     {
       return (mask.value & (1 << target)) != 0;
