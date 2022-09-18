0cc1b5fe67a18f7bc9fbe9db2a1309cc1386f3d3
DiscreteTom <discrete_tom@outlook.com>
2022-09-18 15:30:43 +0800

    feat(general.timer): add SetTimeoutFunc

--- General/Timer/Timer.cs
+++ General/Timer/Timer.cs
@@ -3,18 +3,15 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
-namespace DT.General
-{
-  public class TimeoutHandle
-  {
+namespace DT.General {
+  public class TimeoutHandle {
     Func<bool> finished;
     UnityAction cancel;
     public float startTime { get; private set; }
     public float ttlMs { get; private set; }
     public bool canceled { get; private set; }
 
-    public TimeoutHandle(float startTime, float ttlMs, Func<bool> finished, UnityAction cancel)
-    {
+    public TimeoutHandle(float startTime, float ttlMs, Func<bool> finished, UnityAction cancel) {
       this.startTime = startTime;
       this.ttlMs = ttlMs;
       this.finished = finished;
@@ -22,51 +19,42 @@ namespace DT.General
       this.canceled = false;
     }
 
-    public void Cancel()
-    {
+    public void Cancel() {
       this.cancel.Invoke();
       this.canceled = true;
     }
 
     // just calculate time
     // this might be inaccurate, do NOT use this to check whether task is finished
-    public float Progress()
-    {
+    public float Progress() {
       return (Time.time - this.startTime) * 1000 / this.ttlMs;
     }
 
     // not cancel and not finished
-    public bool Pending()
-    {
+    public bool Pending() {
       return !this.canceled && !this.Finished();
     }
 
-    public bool Finished()
-    {
+    public bool Finished() {
       return this.finished();
     }
 
     // canceled or finished
-    public bool Stopped()
-    {
+    public bool Stopped() {
       return !this.Pending();
     }
   }
 
-  public static class MonoBehaviourExtension
-  {
+  public static class MonoBehaviourExtension {
     // leverage coroutine
     // efficiently call async func
-    public static TimeoutHandle SetTimeout(this MonoBehaviour mb, float ms, UnityAction callback)
-    {
+    public static TimeoutHandle SetTimeout(this MonoBehaviour mb, float ms, UnityAction callback) {
       bool run = true;
       bool finished = false;
 
-      IEnumerator GetCoroutine()
-      {
+      IEnumerator GetCoroutine() {
         yield return new WaitForSeconds(ms / 1000);
-        if (run)
-        {
+        if (run) {
           callback.Invoke();
           finished = true;
         }
@@ -78,31 +66,28 @@ namespace DT.General
     }
   }
 
+  public delegate TimeoutHandle SetTimeoutFunc(float ms, UnityAction cb);
+
   // lazy evaluation
   // useful to calculate progress
-  public class Timer
-  {
+  public class Timer {
     public float startTime { get; private set; }
     public float ttlMs { get; private set; }
 
-    public Timer(float ttlMs)
-    {
+    public Timer(float ttlMs) {
       this.startTime = Time.time;
       this.ttlMs = ttlMs;
     }
 
-    public float Progress()
-    {
+    public float Progress() {
       return (Time.time - this.startTime) * 1000 / this.ttlMs;
     }
 
-    public bool Pending()
-    {
+    public bool Pending() {
       return this.Progress() < 1;
     }
 
-    public bool Finished()
-    {
+    public bool Finished() {
       return !this.Pending();
     }
   }
